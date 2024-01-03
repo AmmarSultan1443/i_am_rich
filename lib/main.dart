ae3d7d99ebae4c9cbad02177ed7c5c5c1427e912
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2024-01-03 21:09:56 +0100

    Start using the Scaffold widget with the appBar implemented

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 // The main function is the starting point for all our Flutter apps.
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child:
-        Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text('I Am Rich'),
+        ),
       ),
     ),
   );
