585d72b93769a029fa4196b2bc0ee6d91732d337
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2024-01-04 12:21:05 +0100

    Here the logo image was updated and an imageAsset for Android was created to make the logo looks well shaped on Android devices.

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         ),
         backgroundColor: Colors.blueGrey,
         body: Center( child: Image(
-          image: NetworkImage(
-              'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg'),
+          image: AssetImage('images/diamond.png'),
         ),
         ),
       ),
